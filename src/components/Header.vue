50e0df05129191b0a13ae80644f26700d4108a08
rowl <airowl.gasga@gmail.com>
2022-03-31 14:44:24 +0200

    creato un select nell'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,13 @@
             <a class="navbar-brand" href="#">
                 <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="">
             </a>
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Seleziona il Genere</option>
+                <option value="1">Rock</option>
+                <option value="2">Pop</option>
+                <option value="3">Jazz</option>
+                <option value="4">Metal</option>
+            </select>
         </nav>
     </header>
 </template>
@@ -19,13 +26,21 @@ export default {
         background-color: #2e3a46;
         padding: 0 1rem;
 
-        a{
-            width: 4rem;
+        nav.navbar{
 
-            img{
-                width: 100%;
+            a{
+                width: 4rem;
+
+                img{
+                    width: 100%;
+                }
+            }
+
+            select{
+                width: 30%;
             }
         }
+        
 
     }
 </style>
