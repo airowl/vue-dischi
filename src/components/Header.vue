7ab112409373b1cd5edfd66ada1bdc52aad987c5
rowl <airowl.gasga@gmail.com>
2022-03-31 18:57:58 +0200

    piccole modifiche

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
             <a class="navbar-brand" href="#">
                 <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="">
             </a>
+
             <select class="form-select" aria-label="Default select example" v-model="selected">
                 <option value="" >Seleziona il Genere</option>
                 <option value="rock" @click="$emit('genre', selected)">Rock</option>
